mordiford/device_xiaomi_sm8250-common

sm8250-common: fod: Add surfaceflinger extension lib
* And specialize it for xiaomi kona devices

Co-authored-by: dataoutputstream <mb150996@gmail.com>
Co-authored-by: luk1337 <priv.luk@gmail.com>

BoardConfigCommon.mk
@@ -74,6 +74,7 @@ SOONG_CONFIG_XIAOMI_KONA_FOD := POS_X POS_Y SIZE
 SOONG_CONFIG_XIAOMI_KONA_FOD_POS_X = 439
 SOONG_CONFIG_XIAOMI_KONA_FOD_POS_Y = 1655
 SOONG_CONFIG_XIAOMI_KONA_FOD_SIZE = 202
+TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension.kona
 
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml

username0136/dump_device_xiaomi_sm8250-common

Change-Id: I440e6d246884fd22044756c6260b3583a1e58a7b

BoardConfigCommon.mk
@@ -69,6 +69,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 ifeq ($(TARGET_HAS_FOD),true)
 SOONG_CONFIG_NAMESPACES += XIAOMI_KONA_FOD
 SOONG_CONFIG_XIAOMI_KONA_FOD := POS_X POS_Y SIZE
+TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension.xiaomi_kona
 endif
 
 # HIDL