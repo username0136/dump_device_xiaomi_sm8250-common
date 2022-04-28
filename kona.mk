LineageOS/android_device_xiaomi_sm8250-common

sm8250-common: touch: add IHighTouchPollingRate
Change-Id: I75e4bc4386eb5ade0fce2c37b2579875670dc9e3

kona.mk
@@ -473,6 +473,10 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0-impl \
     vendor.xiaomi.hardware.touchfeature@1.0-service
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.kona
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti

username0136/dump_device_xiaomi_sm8250-common

kona.mk
@@ -451,6 +451,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_kona
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti