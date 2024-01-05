1b17ac2d551e8e2fc6826d216aea1c5f5f8a0f93
yoriscape <sukhpreetsingh58627@gmail.com>
2024-01-05 14:33:28 +0100

    sm8250-common: Build "android.hardware.power@1.2.vendor"
    in logs
    01-05 04:26:47.636  9317  9317 E vndksupport: Could not load /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so from default namespace: dlopen failed: library "android.hardware.power@1.2.so" not found: needed by /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so in namespace (default)

--- kona.mk
+++ kona.mk
@@ -282,7 +282,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
+    libqti-perfd-client\
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
