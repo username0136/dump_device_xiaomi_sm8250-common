c959f18d92a4781622ad0e127bba58496ad68194
HDzungx <hdzungx@gmail.com>
2024-06-24 02:52:45 +0700

    sm8250-common: Build liblz4.vendor lib
    06-24 02:20:06.597 15269 15269 I android.hardware.camera.provider@2.4-service: CameraProvider@2.4 legacy service is starting.
    06-24 02:20:06.600 15269 15269 E cutils-trace: Error opening trace file: No such file or directory (2)
    06-24 02:20:06.608 15269 15269 E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: library "liblz4.so" not found: needed by /vendor/lib64/hw/camera.qcom.so in namespace (default).
    06-24 02:20:06.608 15269 15269 E HAL     : load: module=/vendor/lib64/hw/camera.qcom.so
    06-24 02:20:06.608 15269 15269 E HAL     : unknown
    06-24 02:20:06.608 15269 15269 E CamPrvdr@2.4-legacy: Could not load camera HAL module: -22 (Invalid argument)
    06-24 02:20:06.608 15269 15269 E android.hardware.camera.provider@2.4-service_64: getProviderImpl: camera provider init failed!

--- kona.mk
+++ kona.mk
@@ -163,6 +163,9 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+PRODUCT_PACKAGES += \
+    liblz4.vendor
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
